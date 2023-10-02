5b57636c90250e7f27b5329fbb9d64a72e49b71c
Mirko Minni <m.minni.1764@vallauri.edu>
2023-10-02 09:40:08 +0200

    Binding

--- src/app/dadi/dadi.component.ts
+++ src/app/dadi/dadi.component.ts
@@ -8,17 +8,16 @@ import { Component } from '@angular/core';
 export class DadiComponent {
   dado1 : number = 5;
   dado2 : number = 5;
-  pathDado1 : string = "assets/img";
-  pathDado2 : string = "assets/img";
+  pathDado1 : string = "assets/img/";
+  pathDado2 : string = "assets/img/";
   pari : boolean = false;
   constructor(){
     this.dado1 = Math.floor(Math.random() * 6)+ 1;
     this.dado2 = Math.floor(Math.random() * 6)+ 1;
-
+    this.pathDado1 += `${this.dado1.toString()}.png`;
+    this.pathDado2 += `${this.dado2.toString()}.png`;
     if(this.dado1 == this.dado2){
       this.pari = true;
-      this.pathDado1 += `${this.dado1.toString()}.png`;
-      this.pathDado2 += `${this.dado2.toString()}.png`;
     }
     else if(this.dado1 > this.dado2){
       this.pari = false;
